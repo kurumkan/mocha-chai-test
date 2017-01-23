35225ac50538f2d2106f4cb133ce0d1550520671
kurumkan <joomrise@gmail.com>
2017-01-23 19:39:48 +0800

    add reducers & tests

--- src/components/comment_box.jsx
+++ src/components/comment_box.jsx
@@ -20,8 +20,11 @@ class CommentBox extends Component {
 
 	handleSubmit(e){
 		e.preventDefault();
-		this.props.saveComment(this.state.comment);
-		this.setState({comment: ''});		
+		var {comment} = this.state;
+		if(comment){
+			this.props.saveComment(comment);
+			this.setState({comment: ''});		
+		}		
 	}	
 
 	render() {
@@ -29,7 +32,9 @@ class CommentBox extends Component {
 			<div className='comment-box'>
 				<form onSubmit={this.handleSubmit.bind(this)}>
 					<textarea onChange={this.handleChange.bind(this)} value={this.state.comment} />
-					<button>Submit Comment</button>
+					<div>
+						<button>Submit Comment</button>
+					</div>	
 				</form>	
 			</div>
 		);
