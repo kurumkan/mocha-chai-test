77b2c2bb28889f1bfcbeba7746bba6b991a8eef4
kurumkan <joomrise@gmail.com>
2017-01-25 00:41:29 +0800

    refactor

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,12 +5,11 @@ import CommentList from './comment_list.jsx';
 export default class App extends Component {	
 
 	render() {  	
-		var comments = ['a','b','c','d'];
 		return (
 			<div>
 				Comments App
 				<CommentBox/>
-				<CommentList comments={comments}/>
+				<CommentList/>
 			</div>
 		);
 	}
